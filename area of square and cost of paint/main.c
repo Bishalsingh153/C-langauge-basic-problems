da2fc4aee9506d8ecb28368c506eaec630002bb9
Bishal Singh <139632789+Bishalsingh153@users.noreply.github.com>
2023-09-03 17:11:11 +0530

    Add files via upload

---
+++ area of square and cost of paint/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int s=14;
+    int area=s*s;
+    int cost=7*area;
+
+    printf("The area of square is %d \n",area);
+    printf("The cost of paint with 7$cm/sq will be: %d$",cost);
+
+}
